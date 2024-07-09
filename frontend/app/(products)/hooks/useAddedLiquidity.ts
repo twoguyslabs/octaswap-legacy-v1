505814c17716bd2b0fef8fa5fe7e73994a0347d8
AL <dreamwrapper@protonmail.com>
2024-07-10 00:41:48 +0700

    remove unused hooks #1

--- frontend/app/(products)/hooks/useAddedLiquidity.ts
+++
@@ -1,9 +0,0 @@
-import { useLocalStorage } from '@uidotdev/usehooks'
-
-export default function useAddedLiquidity() {
-  const [addedLiquidity, setAddedLiquidity] = useLocalStorage<LiquidityPool[]>('addedLiquidity', [])
-
-  const handleAddLiquidity = (pairAddress: `0x${string}` | undefined) => {}
-
-  return { addedLiquidity, setAddedLiquidity }
-}
